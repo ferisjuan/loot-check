5abd506bace69bb5880b2e52c2ad72b122139ccc
Juan Feris <feris.juan@gmail.com>
2020-11-18 11:57:51 -0500

    App Test

---
+++ src/components/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import 'jest-enzyme'
+
+import App from './App'
+
+describe('App', () => {
+	const app = shallow(<App />)
+
+	it('Renders properly', () => {
+		expect(app).toMatchSnapshot()
+	})
+})
