572f95319aed2195950251184d6bc6060e8af0d8
Juan Feris <feris.juan@gmail.com>
2020-11-18 12:34:04 -0500

    Setup Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import rootReducer from './reducers/balance'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './components/App'
 
-render(<App />, document.getElementById('root'))
+const store = createStore(rootReducer)
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
