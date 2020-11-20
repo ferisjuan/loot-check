12481602e9daa0ffbae92c9138b64dfee9c76b83
Juan Feris <feris.juan@gmail.com>
2020-11-20 16:45:51 -0500

    Adding an Input

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -17,4 +17,22 @@ describe('Wallet', () => {
 			`Wallet balance: ${props.balance}😎`
 		)
 	})
+
+	it('creates an input to deposit into or withdraw from the balance', () => {
+		expect(wallet.find('.input-wallet').exists()).toBe(true)
+	})
+
+	describe('when the user types into the wallet input', () => {
+		const userBalance = ' 25'
+
+		beforeEach(() => {
+			wallet
+				.find('.input-wallet')
+				.simulate('change', { target: { value: userBalance } })
+		})
+
+		it('updates the local wallet balance and converts it to a number', () => {
+			expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
+		})
+	})
 })
