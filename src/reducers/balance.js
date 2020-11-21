06091c35c12267885890c9b304c8f248a428db8c
Juan Feris <feris.juan@gmail.com>
2020-11-21 11:02:32 -0500

    Keep Balance in Cookies

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -1,19 +1,31 @@
 import * as constants from '../actions/constants'
+import { read_cookie, bake_cookie } from 'sfcookies'
+
+const BALANCE_COOKIE = 'BALANCE_COOKIE'
 
 const balance = (state = 0, action) => {
+	let balance
+
 	switch (action.type) {
 		case constants.SET_BALANCE:
-			return action.balance
+			balance = action.balance
+			break
 
 		case constants.DEPOSIT:
-			return action.deposit + state
+			balance = action.deposit + state
+			break
 
 		case constants.WITHDRAW:
-			return state - action.withdraw
+			balance = state - action.withdraw
+			break
 
 		default:
-			return state
+			balance = +read_cookie(BALANCE_COOKIE) || state
 	}
+
+	bake_cookie(BALANCE_COOKIE, balance)
+
+	return balance
 }
 
 export default balance
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,13 +1,21 @@
 import * as constants from '../actions/constants'
 import balanceReducer from './balance'
+import balanceReducer2 from './balance'
 
 describe('balanceReducer', () => {
-	it('sets a balance', () => {
+	describe('when initializing', () => {
 		const balance = 10
+		it('sets a balance', () => {
+			expect(
+				balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
+			).toEqual(balance)
+		})
 
-		expect(
-			balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
-		).toEqual(balance)
+		describe('then re-initialize', () => {
+			it('Reads the balance from the cookies', () => {
+				expect(balanceReducer2(undefined, {})).toEqual(balance)
+			})
+		})
 	})
 
 	it('deposits into the balance', () => {
@@ -18,16 +26,16 @@ describe('balanceReducer', () => {
 			balanceReducer(initialState, { type: constants.DEPOSIT, deposit })
 		).toEqual(deposit + initialState)
 	})
-})
 
-it('withdraws from the balance', () => {
-	const withdraw = 20
-	const initialState = 300
+	it('withdraws from the balance', () => {
+		const withdraw = 20
+		const initialState = 300
 
-	expect(
-		balanceReducer(initialState, {
-			type: constants.WITHDRAW,
-			withdraw,
-		})
-	).toEqual(initialState - withdraw)
+		expect(
+			balanceReducer(initialState, {
+				type: constants.WITHDRAW,
+				withdraw,
+			})
+		).toEqual(initialState - withdraw)
+	})
 })
